6848c47280c4eacba71bf8b7b32b70413e6e3d84
Turner Max <voloshok.maksi@ukr.net>
2022-02-18 10:19:20 +0200

    Добавьте файлы проекта.

---
+++ Lab1(1802)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab1_1802_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> number = new List<string>() { "Dima", "Vasya", "Stas", "Oleg", "Semen", "Max" };
+            ParameterizedThreadStart threadstart = new ParameterizedThreadStart(ThreadFunc);
+            Thread thread1 = new Thread(threadstart);
+            thread1.Start((object)number);
+
+            Console.ReadKey();
+
+        }
+
+        static void ThreadFunc(object a)
+        {
+            foreach (var n in (List<string>)a)
+            {
+                Console.WriteLine(n.ToString());
+            }
+
+        }
+    }
+}
